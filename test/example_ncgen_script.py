d1fb3da4f7744ef50a7bcbee1dc75f5c6f0f1fc5
Chris Slocum <Christopher.Slocum@colostate.edu>
2019-05-22 19:16:44 +0000

    changed both examples to toml

--- test/example_ncgen_script.py
+++ test/example_ncgen_script.py
@@ -28,28 +28,29 @@ lons = np.linspace(0, 359)
 time = np.array([123456, 123457], dtype=np.int32)
 random_data = np.random.random((time.size, lats.size, lons.size)) * 75.
 
+#####################################################################
 # First example: We have all our data and just want a NetCDF file
 data = {'lat': lats,
         'lon': lons,
         'time': time,
         'pwat': random_data}
 # Set  the example configuration file
-nc_config = "./example_nc_config.json"
+nc_config = "./example_nc_config.toml"
 # Call ncgen
 ncgen('all_data_ex.nc', data, nc_config, clobber=True)
 
+####################################################################
 # Second example: We don't have all our data or we will append in time
 data = {'lat': lats,
         'lon': lons}
 
-nc_config = "./example_nc_config_timeUnlimited.json"
-nc_fid = ncgen('unlimited_time_ex.nc', data, nc_config, return_instance=True, clobber=True)
-# ncgen returns an instance of the NetCDF file.
-# To write to the unlimited dimensions, use a procedure like
-# the one provided below.
-for count in range(time.size):
-    # increase the time dimension by one by writing a new time
-    nc_fid.variables['time'][count] = time[count]
-    # write data corresponding to that time
-    nc_fid.variables['pwat'][count, :, :] = random_data[count, :, :]
-nc_fid.close()
+nc_config = "./example_nc_config_timeUnlimited.toml"
+# ncgen returns an instance of the NetCDF file and we will use a 'with' statement
+with ncgen('unlimited_time_ex.nc', data, nc_config, return_instance=True, clobber=True) as nc_fid:
+    # To write to the unlimited dimensions, use a procedure like
+    # the one provided below.
+    for count in range(time.size):
+        # increase the time dimension by one by writing a new time
+        nc_fid.variables['time'][count] = time[count]
+        # write data corresponding to that time
+        nc_fid.variables['pwat'][count, :, :] = random_data[count, :, :]
