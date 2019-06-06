c6dbcc6a2394284ca13bfcec8118cd74a2f928a2
Chris Slocum <Christopher.Slocum@colostate.edu>
2019-06-06 21:31:18 +0000

    added ability to do one level of groups

--- netCDF_utilities/ncgen.py
+++ netCDF_utilities/ncgen.py
@@ -69,31 +69,47 @@ def ncgen(filename, data, nc_config, nc_format='NETCDF4',
             nc_config = toml.load(open(nc_config, 'r'), _dict=OrderedDict)
         else:
             raise IOError("The following file extension for the configuration file is not supported: " + ext)
-    nc_attr = nc_config['global_attributes']
-    nc_dims = nc_config['dimensions']
-    nc_vars = nc_config['variables']
     nc_fid = netCDF4.Dataset(filename, mode='w', clobber=clobber,
                              format=nc_format)
-    for global_attr in nc_attr:
+    nc_attrs = nc_config['global_attributes']
+    for global_attr in nc_attrs:
         if global_attr not in _standard_global_attr:
-            warnings.warn("%s not in list of standard global attributes" %
-                          global_attr)
-        setattr(nc_fid, global_attr, nc_attr[global_attr])
+            warnings.warn("%s not in list of standard global attributes" % global_attr)
+        setattr(nc_fid, global_attr, nc_attrs[global_attr])
     nc_fid.history = "Generated %s" % datetime.datetime.utcnow()
+    if 'groups' in nc_config:
+        for groupname in nc_config['groups']:
+            group = nc_fid.createGroup(groupname)
+            _add_to_group(group, data['groups'][groupname], nc_config['groups'][groupname], nc_format)
+    else:
+        _add_to_group(nc_fid, data, nc_config, nc_format)
+    nc_fid.close()
+    if return_instance:
+        nc_fid = netCDF4.Dataset(filename, mode='a')
+        return nc_fid
+
+
+def _add_to_group(group, data, config, nc_format):
+    if 'attributes' in config:
+        attrs = config['attributes']
+        for attr in attrs:
+            group.setncattr(attr, attrs[attr])
+    nc_dims = config['dimensions']
     for dim in nc_dims:
         ncattrs = list(nc_dims[dim].keys())
         if 'size' in ncattrs:
-            nc_fid.createDimension(dim, nc_dims[dim]['size'])
+            group.createDimension(dim, nc_dims[dim]['size'])
         elif 'dat' in ncattrs:
-            nc_fid.createDimension(dim, data[nc_dims[dim]['dat']].size)
+            group.createDimension(dim, data[nc_dims[dim]['dat']].size)
         else:
-            nc_fid.createDimension(dim, None)
-        nc_dim = _create_var(nc_fid, name=dim, dtype=np.dtype(nc_dims[dim]['dtype']), dimensions=(dim), attributes=nc_dims[dim])
+            group.createDimension(dim, None)
+        nc_dim = _create_var(group, name=dim, dtype=np.dtype(nc_dims[dim]['dtype']), dimensions=(dim), attributes=nc_dims[dim])
         for ncattr in ncattrs:
             if ncattr not in _NOT_ATTRS:
                 nc_dim.setncattr(ncattr, nc_dims[dim][ncattr])
             elif ncattr == 'dat':
-                nc_fid.variables[dim][:] = data[nc_dims[dim]['dat']]
+                group.variables[dim][:] = data[nc_dims[dim]['dat']]
+    nc_vars = config['variables']
     for var in nc_vars:
         if nc_vars[var]['dtype'] == 'str':
             if nc_format != 'NETCDF4':
@@ -107,16 +123,16 @@ def ncgen(filename, data, nc_config, nc_format='NETCDF4',
             dimensions = nc_vars[var]['dim']
             assert all(dim in nc_dims for dim in dimensions), \
                 "One of the dimensions for %s does not exist" % var
-            nc_var = _create_var(nc_fid, name=var, dtype=dtype, dimensions=(dimensions), attributes=nc_vars[var])
+            nc_var = _create_var(group, name=var, dtype=dtype, dimensions=(dimensions), attributes=nc_vars[var])
         else:
             if dtype == 'c':
                 size = len(data[var])
                 name = "strdim%02d" % size
                 if name not in nc_fid.variables:
-                    nc_fid.createDimension(name, size)
-                nc_var = _create_var(nc_fid, name=var, dtype=dtype, dimensions=(name,), attributes=nc_vars[var])
+                    group.createDimension(name, size)
+                nc_var = _create_var(group, name=var, dtype=dtype, dimensions=(name,), attributes=nc_vars[var])
             else:
-                nc_var = _create_var(nc_fid, name=var, dtype=dtype, attributes=nc_vars[var])
+                nc_var = _create_var(group, name=var, dtype=dtype, attributes=nc_vars[var])
         for ncattr in list(nc_vars[var].keys()):
             if ncattr not in _NOT_ATTRS:
                 nc_var.setncattr(ncattr, nc_vars[var][ncattr])
@@ -124,14 +140,10 @@ def ncgen(filename, data, nc_config, nc_format='NETCDF4',
             if dtype == 'c':
                 data_entry = netCDF4.stringtoarr(data[var], len(data[var]))
                 for count, char in enumerate(data[var]):
-                    nc_fid.variables[var][count] = data_entry[count]
+                    group.variables[var][count] = data_entry[count]
             else:
                 data_entry = data[var]
                 if has_dim:
-                    nc_fid.variables[var][:] = data_entry
+                    group.variables[var][:] = data_entry
                 else:
-                    nc_fid.variables[var][0] = data_entry
-    nc_fid.close()
-    if return_instance:
-        nc_fid = netCDF4.Dataset(filename, mode='a')
-        return nc_fid
+                    group.variables[var][0] = data_entry
