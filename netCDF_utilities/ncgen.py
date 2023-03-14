ab3cb1bc28e303b511dd2e5d46da18f7ca853c12
Chris Slocum <christopher.slocum@colostate.edu>
2023-03-14 03:55:59 +0000

    removed alias to np.int

--- netCDF_utilities/ncgen.py
+++ netCDF_utilities/ncgen.py
@@ -52,7 +52,7 @@ _NC4_OPTIONS = [
 _NOT_ATTRS = ['size', 'dtype', 'dat', 'dim', 'var'] + _NC4_OPTIONS
 
 # Tuples of types
-_SCALAR_TYPES = (float, int, np.float32, np.float64, np.int)
+_SCALAR_TYPES = (float, int, np.float32, np.float64, np.int16, np.int32, np.int64)
 _ARRAY_TYPES = (np.ndarray, np.ma.core.MaskedArray, list, tuple)
 _STR_TYPES = (str, np.str, np.character, np.unicode)
 
