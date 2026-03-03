5706624dab719775336a248dd5ca945e05dca183
Chris Slocum <Christopher.Slocum@colostate.edu>
2026-03-03 17:37:51 +0000

    Change to f-strings

--- netCDF_utilities/ncdump.py
+++ netCDF_utilities/ncdump.py
@@ -41,10 +41,10 @@ def ncdump(nc_fid: NCtDsetGrp, verb: bool = True) -> tuple[list, list, list]:
         try:
             print("\t\ttype:", repr(nc_fid.variables[key].dtype))
             for ncattr in nc_fid.variables[key].ncattrs():
-                print('\t\t%s:' % ncattr,
+                print(f"\t\t{ncattr}:",
                       repr(nc_fid.variables[key].getncattr(ncattr)))
         except KeyError:
-            print("\t\tWARNING: %s does not contain variable attributes" % key)
+            print(f"\t\tWARNING: {key} does not contain variable attributes")
 
     # NetCDF global attributes
     nc_attrs = nc_fid.ncattrs()
@@ -53,7 +53,7 @@ def ncdump(nc_fid: NCtDsetGrp, verb: bool = True) -> tuple[list, list, list]:
     if verb:
         print("NetCDF Global Attributes:")
         for nc_attr in nc_attrs:
-            print('\t%s:' % nc_attr, repr(nc_fid.getncattr(nc_attr)))
+            print(f"\t{nc_attr}:", repr(nc_fid.getncattr(nc_attr)))
     nc_dims = list(nc_fid.dimensions.keys())  # list of nc dimensions
     # Dimension shape information.
     if verb:
@@ -68,7 +68,7 @@ def ncdump(nc_fid: NCtDsetGrp, verb: bool = True) -> tuple[list, list, list]:
         print("NetCDF variable information:")
         for var in nc_vars:
             if var not in nc_dims:
-                print('\tName:', var)
+                print("\tName:", var)
                 print("\t\tdimensions:", nc_fid.variables[var].dimensions)
                 print("\t\tsize:", nc_fid.variables[var].size)
                 print_ncattr(var)
