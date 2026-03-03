c143b58429018fd2da70a8bb86e709de2d8119f0
Chris Slocum <Christopher.Slocum@colostate.edu>
2026-03-03 17:01:55 +0000

    Add docstring to dunder init

--- netCDF_utilities/__init__.py
+++ netCDF_utilities/__init__.py
@@ -1,6 +1,7 @@
-from __future__ import division, print_function, absolute_import
+"""Utility to provide ncdump and ncgen."""
+# Local folder imports
 from .ncgen import ncgen
 from .ncdump import ncdump
 
-__all__ = ['ncgen', 'ncdump']
-__version__ = '1.6'
+__all__ = ["ncgen", 'ncdump']
+__version__ = "1.6"
